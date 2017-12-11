e7127f7cdc981fed3b61abc9ffeced26134f2b4b
nclsmitchell <ncls.mitchell@gmail.com>
2017-12-11 12:11:39 +0100

    update server.py

--- src/server/server.py
+++ src/server/server.py
@@ -22,7 +22,7 @@ def download_file(path):
     try:
         return send_file(os.getcwd() + '/src/server/files/%s.csv' % path, attachment_filename='%s.csv' % path)
     except Exception as e:
-		return e
+		print e
 
 class ErrorExporter(Resource):
 
