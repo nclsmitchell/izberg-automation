26ae176e6b1c072b47cf67112ae7b0c95b531bf3
nclsmitchell <ncls.mitchell@gmail.com>
2017-12-13 17:06:28 +0100

    add node server

---
+++ src/server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, (error) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.info('Server started');
+    }
+});
+
+app.use('/static', express.static(path.join(__dirname, './build/static')));
+
+app.get('*', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, './build/index.html'));
+});
+
+module.exports = app;
